27b71b581dd1f017882e8c32b091e5a91e7d8800
Paper Krane <20448279+paper-krane@users.noreply.github.com>
2022-12-15 21:29:19 -0700

    Updated Paths

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@ import { RouterView } from 'vue-router';
 // Components
 import HeaderVue from '@/components/Header/Header.vue';
 import CursorVue from '@/components/Utilities/Cursor.vue';
+import HeroBanner from '@/components/Pages/Index/HeroBanner.vue';
+import Services from '@/components/Pages/Index/Services.vue';
+import CallToAction from '@/components/Pages/Index/CallToAction.vue';
+import RecentArticles from '@/components/Pages/Index/RecentArticles.vue';
+import Faqs from '@/components/Pages/Index/Faqs.vue';
+
 
 // Variables
 const navStatus = ref(false);
@@ -19,7 +25,11 @@ provide('navStatus', navStatus);
     <HeaderVue />
     <div id="vi__page" ref="page">
         <main id="vi__content">
-            <RouterView />
+            <HeroBanner />
+            <Services />
+            <RecentArticles />
+            <!-- <Faqs /> -->
+            <CallToAction />
         </main>
         <footer id="vi__footer">
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,11 +11,11 @@
 </template>
 
 <script setup>
-import HeroBanner from '/src/components/Pages/Index/HeroBanner.vue';
-import Services from '/src/components/Pages/Index/Services.vue';
-import CallToAction from '/src/components/Pages/Index/CallToAction.vue';
-import RecentArticles from '/src/components/Pages/Index/RecentArticles.vue';
-import Faqs from '/src/components/Pages/Index/Faqs.vue';
+import HeroBanner from '@/components/Pages/Index/HeroBanner.vue';
+import Services from '@/components/Pages/Index/Services.vue';
+import CallToAction from '@/components/Pages/Index/CallToAction.vue';
+import RecentArticles from '@/components/Pages/Index/RecentArticles.vue';
+import Faqs from '@/components/Pages/Index/Faqs.vue';
 
 
 </script>
